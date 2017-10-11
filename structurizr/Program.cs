412c594f4f8b05e9aad09d9405a4df5611cff90c
Simon Brown <simon.brown@codingthearchitecture.com>
2017-10-11 18:00:21 +0100

    Initial commit.

---
+++ structurizr/Program.cs
@@ -0,0 +1,59 @@
+﻿using Structurizr;
+using Structurizr.Api;
+using Structurizr.Documentation;
+
+namespace structurizr
+{
+    
+    /// <summary>
+    /// This is a simple example of how to get started with Structurizr for .NET.
+    /// </summary>
+    class Program
+    {
+
+        private const long WorkspaceId = 1234;
+        private const string ApiKey = "key";
+        private const string ApiSecret = "secret";
+
+        static void Main(string[] args)
+        {
+            // a Structurizr workspace is the wrapper for a software architecture model, views and documentation
+            Workspace workspace = new Workspace("Getting Started", "This is a model of my software system.");
+            Model model = workspace.Model;
+
+            // add some elements to your software architecture model
+            Person user = model.AddPerson("User", "A user of my software system.");
+            SoftwareSystem softwareSystem = model.AddSoftwareSystem("Software System", "My software system.");
+            user.Uses(softwareSystem, "Uses");
+
+            // define some views (the diagrams you would like to see)
+            ViewSet views = workspace.Views;
+            SystemContextView contextView = views.CreateSystemContextView(softwareSystem, "SystemContext",
+                "An example of a System Context diagram.");
+            contextView.PaperSize = PaperSize.A5_Landscape;
+            contextView.AddAllSoftwareSystems();
+            contextView.AddAllPeople();
+
+            // add some documentation
+            StructurizrDocumentationTemplate template = new StructurizrDocumentationTemplate(workspace);
+            template.AddContextSection(softwareSystem, Format.Markdown,
+                "Here is some context about the software system...\n" +
+                "\n" +
+                "![](embed:SystemContext)");
+
+            // add some styling
+            Styles styles = views.Configuration.Styles;
+            styles.Add(new ElementStyle(Tags.SoftwareSystem) { Background = "#1168bd", Color = "#ffffff" });
+            styles.Add(new ElementStyle(Tags.Person) { Background = "#08427b", Color = "#ffffff", Shape = Shape.Person });
+
+            uploadWorkspaceToStructurizr(workspace);
+        }
+
+        private static void uploadWorkspaceToStructurizr(Workspace workspace) {
+            StructurizrClient structurizrClient = new StructurizrClient(ApiKey, ApiSecret);
+            structurizrClient.PutWorkspace(WorkspaceId, workspace);
+        }
+
+    }
+
+}
